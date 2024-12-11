551d1532d2c0f5ad0c6de3be73e9e8d5b25ea3d0
XLLAMAA <160739250+XLLAMAA@users.noreply.github.com>
2024-12-11 10:29:40 +0100

    Create INTERFAZSONIDO.java

---
+++ INTERFAZSONIDO.java
@@ -0,0 +1,9 @@
+package MIPARTE;
+
+public interface INTERFAZSONIDO {
+
+	public void sonido();	//REPRODUCE EL SONIDO UNA SOLA VEZ
+	public void sonidoBucle(); //REPRODUCE EL SONIDO EN BUCLE
+	public void detenerSonido();	//DETIENE EL SONIDO
+	
+}
