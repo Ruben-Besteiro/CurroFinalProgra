d7e2294c2158c45ccffb62b6a3d94280ae5ac1b4
XLLAMAA <160739250+XLLAMAA@users.noreply.github.com>
2024-12-13 10:01:41 +0100

    Update ModoSolo.java

--- ModoSolo.java
+++ ModoSolo.java
@@ -1,26 +1,30 @@
-package CurroFinal;
+package MIPARTE;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class ModoSolo {
-
-    // Constructor
+	
+    //CONSTRUCTOR
     public ModoSolo() {
         cambiarPantalla();
+        //MenuPausa();
+        
     }
 
-    // Métodos
+    //METODOS
     public void cambiarPantalla() {
 
         MenuJuego.Panel.setBackground(Color.BLUE); // COLOR DE FONDO EL MISMO    
 
-        // Botón ATACAR
+        //BOTON ATACAR
         JButton boton1 = new JButton("ATACAR");
         boton1.setBackground(Color.WHITE);
         boton1.setForeground(Color.RED);
@@ -29,12 +33,11 @@ public class ModoSolo {
 
         boton1.addActionListener(new ActionListener() { // CUANDO SE PULSE EL BOTON DE SOLO QUE SE VAYA A LA CLASE PERSONAJE Y ATACA
             public void actionPerformed(ActionEvent e) {
-                Personaje pers = new Personaje();
-                pers.ataque();
+            	generarOpcionesAtaque();
             }
         });
 
-        // Botón HABILIDAD
+        //BOTON HABILIDAD
         JButton boton2 = new JButton("HABILIDAD");
         boton2.setBackground(Color.WHITE);
         boton2.setForeground(Color.RED);
@@ -43,12 +46,11 @@ public class ModoSolo {
 
         boton2.addActionListener(new ActionListener() { // CUANDO SE PULSE EL BOTON DE SOLO QUE SE VAYA A LA CLASE PERSONAJE Y ATACA
             public void actionPerformed(ActionEvent e) {
-                Personaje pers = new Personaje();
-                // FALTA LLAMAR A LO QUE HACE
+            	CrearPersonajes.personajes.get(0).habilidad();	//EL 0 ES POR EL NUMERO DEL PERSONAJE ES DECIR EL NUESTRO POR QUE ES EN SOLITARIO
             }
         });
 
-        // Botón IMPRIMIR PERSONAJES
+        //BOTON PARA IMPRIMIR TODOS LOS JUGADORES
         JButton botonImprimir = new JButton("IMPRIMIR PERSONAJES");
         botonImprimir.setBackground(Color.WHITE);
         botonImprimir.setForeground(Color.RED);
@@ -61,7 +63,7 @@ public class ModoSolo {
             }
         });
 
-        // Añadir los botones al panel
+        //AÑADO BOTONES Y TODO
         MenuJuego.Panel.add(boton1);
         MenuJuego.Panel.add(boton2);
         MenuJuego.Panel.add(botonImprimir);
@@ -86,9 +88,29 @@ public class ModoSolo {
 
         if (eleccion == 0) {
             System.out.println("Bot ha elegido atacar y ha pulsado el boton");
+            CrearPersonajes.personajes.get(1).ataque(2);	//ESCOGES A QUIEN ATACAS
         } else {
             System.out.println("Bot ha elegido habilidad y ha pulsado el boton");
+            CrearPersonajes.personajes.get(1).habilidad();	//ESCOGES DESPUES TE SALE UN PANEL PARA QUE ELIJAS
         }
     }
+    
+    //METODO PARA ELIMINAR JUGADOR
+    public void eliminarJugador() {
+    	CrearPersonajes.personajes.removeIf(personaje -> personaje.getVida() <= 0);
+        System.out.println("Se han eliminado los jugadores con vida menor o igual a 0.");
+     }
+    
+    
+    //METODO PARA ELEGUIR AL JUGADOR AL CUAL ATACAR SIEMPRE QUE TENGA MAS DE 0 DE VIDA
+    private String[] generarOpcionesAtaque() {
+        ArrayList<String> opciones = new ArrayList<>();	//ARRAY LIST DE LAS OPCIONES QUE TIENE EL JUADOR HUMANO
+        for (int i = 1; i < CrearPersonajes.personajes.size(); i++) { //INT I=1 PARA QUE SE SALTE A MI JUGADOR QUE ES EL 0
+        	if (CrearPersonajes.personajes.get(i).getVida() > 0) {
+                opciones.add("Atacar a " + i);
+            }
+        }
+        eliminarJugador();
+        return opciones.toArray(new String[0]);
+    }
 }
-
